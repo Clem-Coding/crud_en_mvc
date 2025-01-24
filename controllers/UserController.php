449decd6e85ab5979b314cbc388d8c759537f0b3
Clem-Coding <clementine.blondeau@gmail.com>
2025-01-24 10:16:56 +0100

    feat : add Step 9

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -19,7 +19,21 @@ class UserController
         $route = "create_user";
         require "templates/layout.phtml";
     }
-    public function checkCreate(): void {}
+    public function checkCreate(): void
+    {
+
+        if (isset($_POST["first_name"]) && isset($_POST["last_name"]) && isset($_POST["email"])) {
+            $firstName = $_POST["first_name"];
+            $lastName = $_POST["last_name"];
+            $email = $_POST["email"];
+        }
+
+        $user = new User($email, $firstName, $lastName);
+        $userManager = new UserManager();
+        $userManager->create($user);
+
+        header("Location: index.php");
+    }
 
 
     public function update(): void
